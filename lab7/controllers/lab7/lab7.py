80d301d9a3eb9d216d9fe97d235c6fda941f1a6e
O11WL1D <gavinunrue@gmail.com>
2026-04-23 15:20:00 -0600

    default commit

--- lab7/controllers/lab7/lab7.py
+++ lab7/controllers/lab7/lab7.py
@@ -269,19 +269,19 @@ def compute_potential_field_reinit(robot_x, robot_y, goal_x, goal_y, lidar_range
 
             object_position_x= (reading_to_meters*lidar_ranges[i])*math.cos(radians_per_measurement*i)
             object_position_y= (reading_to_meters*lidar_ranges[i])*math.cos(radians_per_measurement*i)
-            print("OBJECT POSITION " + str(object_position_x) + "  " +str(object_position_y) )
+            #print("OBJECT POSITION " + str(object_position_x) + "  " +str(object_position_y) )
             
             if(not((reading_to_meters*lidar_ranges[i])>D0)):
 
                 #repulsive potential
-                dx+=((1/2)*K_REP)* (1/(abs(robot_x-object_position_x)^2))
-                dy+=((1/2)*K_REP)* (1/(abs(robot_y-object_position_y)^2))
+                dx-=((1/2)*K_REP)* (1/(abs(robot_x-object_position_x)**2))
+                dy-=((1/2)*K_REP)* (1/(abs(robot_y-object_position_y)**2))
 
 
   
         #attractive potential 
-        dx+=((1/2)*K_ATT)* ((abs(robot_x-goal_x)^2))
-        dy+=((1/2)*K_ATT)* ((abs(robot_y-goal_y)^2))
+        dx+=((1/2)*K_ATT)* ((float(abs(robot_x-goal_x))**2))
+        dy+=((1/2)*K_ATT)* ((float(abs(robot_y-goal_y)**2)))
         return np.array([dx, dy], dtype=float)
 
 
